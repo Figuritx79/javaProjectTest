ac0427b6947ab26c47bf80b8a8b6f060e4ffc00d
Enrique Gonzalez <gonherenrique@gmail.com>
2023-09-26 08:10:17 -0600

    Ingresado dos numeros para sumar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("Introduce el primer numero para sumar");
+        int numberOne = scanner.nextInt();
+        System.out.println("Introduce el segundo numero para sumar");
+        int numberTwo = scanner.nextInt();
+        System.out.println(numberOne + numberTwo);
+        scanner.close();
     }
 }
