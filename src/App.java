b491fd78693c1c54db5db3f7e5ca0b22943f1000
Enrique Gonzalez <gonherenrique@gmail.com>
2023-09-26 08:29:33 -0600

    Agregado suma,resta,multiplicacion y division

--- src/App.java
+++ src/App.java
@@ -2,12 +2,27 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+
+        //Suma de dos numeros
+       /*  Scanner scanner = new Scanner(System.in);
          System.out.println("Introduce el primer numero para sumar");
         int numberOne = scanner.nextInt();
         System.out.println("Introduce el segundo numero para sumar");
         int numberTwo = scanner.nextInt();
         System.out.println(numberOne + numberTwo);
+        scanner.close(); */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el primer numero para sumar");
+        float numberOne = scanner.nextFloat();
+        System.out.println("Introduce el segundo numero para sumar");
+        float numberTwo = scanner.nextFloat();
+        System.out.println("Suma: "+(numberOne + numberTwo));
+        System.out.println("Resta: "+(numberOne-numberTwo));
+        System.out.println("Multiplicacion: "+(numberOne*numberTwo));
+        System.out.println("Divsion: "+(numberOne/numberTwo));
         scanner.close();
+ 
+
     }
 }
